5897cfaf20eac3e4e10fbd3230b09d41c3dfaf59
Abhay Verma <abhayoct04@gmail.com>
2026-03-19 10:13:10 +0530

    Modified NewsItem Component

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2 className='my-4'>NewsApp - Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription"/>
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="MyTitle" description="MyDescription"/>
+          </div>
+        </div>
       </div>
     )
   }
